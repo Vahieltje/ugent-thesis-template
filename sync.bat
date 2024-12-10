4879c1a85d577bc29f1a29ebc26210086d5d210e
tvgiel <Thomas.VanGiel@UGent.be>
2024-12-10 09:52:59 +0100

    Automated commit - 10/12/2024  9:52:59,62

--- sync.bat
+++ sync.bat
@@ -1,6 +1,8 @@
 @echo off
-cd "C:\Users\tvgiel\AppData\Roaming\typst\packages\local\ugent-thesis-template"
+echo Script started >> C:\Users\tvgiel\Scripts\sync_log.txt
+cd "C:\Users\tvgiel\AppData\Roaming\typst\packages\local\ugent-thesis-template" || exit
 git add .
 git commit -m "Automated commit - %date% %time%"
 git pull origin main
 git push origin main
+echo Script finished >> "C:\Users\tvgiel\AppData\Roaming\typst\packages\local\ugent-thesis-template\sync_log.txt"
